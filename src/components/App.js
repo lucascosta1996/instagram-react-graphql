bb7b547c5038a98b7567ea620f07209b97355620
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-05-01 21:11:39 -0300

    Add ApolloClient config to graphql api

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
+import Header from "./Header.js";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
 
 function App() {
+  const httpLink = new HttpLink({
+    uri: "https://instagram-graphql-react.herokuapp.com/v1/graphql"
+  });
+
+  const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+  });
+
   return (
-    <div className="App">
-    </div>
+    <ApolloProvider client={client}>
+           <Header />
+    </ApolloProvider>
   );
 }
 
